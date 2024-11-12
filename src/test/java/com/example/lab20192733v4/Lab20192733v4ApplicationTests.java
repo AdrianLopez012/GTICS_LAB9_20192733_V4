ee16d565e413d4b4508576cfa7c2db0b43c41618
AdrianLopez012 <142950326+AdrianLopez012@users.noreply.github.com>
2024-11-11 21:59:34 -0500

    Initial commit with base project structure

---
+++ src/test/java/com/example/lab20192733v4/Lab20192733v4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab20192733v4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab20192733v4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
