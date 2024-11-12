3374141bd8b5987d595d6331067ae5bc22874689
AdrianLopez012 <142950326+AdrianLopez012@users.noreply.github.com>
2024-11-11 21:53:08 -0500

    Add files via upload

---
+++ lab20192733v4/src/main/java/com/example/lab20192733v4/Lab20192733v4Application.java
@@ -0,0 +1,13 @@
+package com.example.lab20192733v4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab20192733v4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab20192733v4Application.class, args);
+	}
+
+}
